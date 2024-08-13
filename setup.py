b886b7b5a5a5d6a4bff0bc380a7c49411543cda6
DeadBoy <61543491+D34DBOY@users.noreply.github.com>
2024-08-13 17:24:12 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="verifyTelegramwebapp",
-    version="0.1.0", 
+    version="1.0", 
     author="Mohammad Mohammadi Bijaneh", 
     author_email="hiostad6@gmail.com", 
     description="This Python module securely validates and extracts user data from a URL-encoded string, using HMAC and SHA-256 for data integrity. It features a User class to structure user information and a Data class for validation and data parsing. Ideal for web applications needing secure user authentication and data management, it's easy to integrate and use.",
